66f4601eed974c5ee786dedb6d6cba2f5025d9e7
simondegheselle <simondegheselle@hotmail.com>
2018-05-17 13:04:22 +0200

    Update 3.asm

--- reeks8/3.asm
+++ reeks8/3.asm
@@ -2,12 +2,29 @@ verm: mov 33H,#00H
   mov 34H, #00H
   mov 30H, #251d
   mov 31H, #254d
+  mov A, 30H
 loop:
   clr C
-  mov A, 30H
   rrc A
   mov 30H, A
   jnc verder
+  clr C
+  
   mov A, 31H
   add A, 33H
-  
+  mov 33H, A
+  mov A, 32H
+  addc A, 34H
+  mov 34H, A
+verder: clr C
+  mov A, 31H
+  rlc 
+  mov 31H, A
+  mov A, 32H
+  rlc A 
+  mov 32H, A
+  mov A, 30H
+  jnz loop
+  mov A, 33H
+  mov B, 34H
+  ret
