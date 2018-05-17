1ca4039ecd9feb8a116fde36fc4be98d6881c7a1
simondegheselle <simondegheselle@hotmail.com>
2018-05-17 14:29:56 +0200

    Create 6.asm

---
+++ reeks8/6.asm
@@ -0,0 +1,31 @@
+; getal in A vermenigvuldigen met 62 en als high byte register B gebruiken
+$include (c8051f120.inc)
+
+cseg at 0000H 
+	jmp main
+
+cseg at 0050H
+main:
+	clr EA
+	mov WDTCN, #0DEh
+	mov WDTCN, #0ADh
+	setb EA
+	mov A, #02d
+	push Acc
+	
+	mov R1, #06d
+loop: 
+	clr C
+	rlc A
+	push Acc
+	mov A, B
+	rlc A
+	pop Acc
+	djnz R1, loop
+
+	pop 00h
+
+	subb A, R0
+	subb A, R0
+
+	jmp $
